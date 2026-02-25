04d01b080e7050c1318e1b3b5dd8fce590504b82
FranckA1 <70436407+FranckA1@users.noreply.github.com>
2026-02-25 21:40:21 +0000

    Create config/database.js file for MySQL database connection

---
+++ config/database.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'your_username',
+    password: 'your_password',
+    database: 'your_database_name'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database: ' + err.stack);
+        return;
+    }
+    console.log('Connected to the database with id ' + connection.threadId);
+});
+
+module.exports = connection;
