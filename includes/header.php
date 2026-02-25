af038eeabd60117a5c10eda3d1e6a061e99ae3e0
FranckA1 <70436407+FranckA1@users.noreply.github.com>
2026-02-25 22:06:12 +0000

    Create header.php file with site header and navigation code

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<?php
+// Site Header and Navigation
+
+function display_header() {
+    echo '<header>';
+    echo '<h1>Welcome to LeaderTech Blog</h1>';
+    echo '<nav>';
+    echo '<ul>';
+    echo '<li><a href="index.php">Home</a></li>';
+    echo '<li><a href="about.php">About</a></li>';
+    echo '<li><a href="contact.php">Contact</a></li>';
+    echo '</ul>';
+    echo '</nav>';
+    echo '</header>';
+}
+?>
